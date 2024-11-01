75899dd6a03a27947473345bc73c20b3cd1a786e
Thomas Milin <tmilin@made-f0Dr9s19.clusters.42paris.fr>
2024-11-01 16:35:12 +0100

    42

---
+++ philosophers.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmilin <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/01 16:30:09 by tmilin            #+#    #+#             */
+/*   Updated: 2024/11/01 16:33:37 by tmilin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <string.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
