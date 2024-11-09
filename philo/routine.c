9f5c2dc5ad331f1c8f43b41e07aab7c53a8c763e
gkcdq <thomasmilin1@gmail.com>
2024-11-09 05:59:49 +0100

    home

--- philo/routine.c
+++ philo/routine.c
@@ -49,24 +49,36 @@ void	after_taking_forks_impair(t_philo *philo, t_params *params, long int c_time
 		philo->last_eat = get_current_time();
 		usleep(params->eat_time * 1000);
 		philo->count_eat += 1;
-		pthread_mutex_lock(&params->protect_printf);
-		c_time = get_current_time() - params->start_time;
+		pthread_mutex_lock(&params->protect_dead);
 		if (params->p_dead == 0)
+		{
+			pthread_mutex_lock(&params->protect_printf);
+			c_time = get_current_time() - params->start_time;
 			printf("%ld %d is eating\n", c_time, philo->index);
-		pthread_mutex_unlock(&params->protect_printf);
+			pthread_mutex_unlock(&params->protect_printf);
+		}
+		pthread_mutex_unlock(&params->protect_dead);
 		pthread_mutex_unlock(philo->right_fork);
 		pthread_mutex_unlock(philo->left_fork);
-		pthread_mutex_lock(&params->protect_printf);
-		c_time = get_current_time() - params->start_time;
+		pthread_mutex_lock(&params->protect_dead);
 		if (params->p_dead == 0)
+		{
+			pthread_mutex_lock(&params->protect_printf);
+			c_time = get_current_time() - params->start_time;
 			printf("%ld %d is sleeping\n", c_time, philo->index);
-		pthread_mutex_unlock(&params->protect_printf);
+			pthread_mutex_unlock(&params->protect_printf);
+		}
+		pthread_mutex_unlock(&params->protect_dead);
 		usleep(params->sleep_time * 1000);
-		pthread_mutex_lock(&params->protect_printf);
-		c_time = get_current_time() - params->start_time;
+		pthread_mutex_lock(&params->protect_dead);
 		if (params->p_dead == 0)
+		{
+			pthread_mutex_lock(&params->protect_printf);
+			c_time = get_current_time() - params->start_time;
 			printf("%ld %d is thinking\n", c_time, philo->index);
-		pthread_mutex_unlock(&params->protect_printf);
+			pthread_mutex_unlock(&params->protect_printf);
+		}
+		pthread_mutex_unlock(&params->protect_dead);
 
 }
 
@@ -78,25 +90,37 @@ void	after_taking_forks(t_philo *philo, t_params *params, long int c_time)
 		{
 			philo->last_eat = get_current_time();
 			philo->count_eat += 1;
-			pthread_mutex_lock(&params->protect_printf);
-			c_time = get_current_time() - params->start_time;
+			pthread_mutex_lock(&params->protect_dead);
 			if (params->p_dead == 0)
+			{
+				pthread_mutex_lock(&params->protect_printf);
+				c_time = get_current_time() - params->start_time;
 				printf("%ld %d is eating\n", c_time, philo->index);
-			pthread_mutex_unlock(&params->protect_printf);
+				pthread_mutex_unlock(&params->protect_printf);
+			}
+			pthread_mutex_unlock(&params->protect_dead);
 			pthread_mutex_unlock(philo->left_fork);
 			pthread_mutex_unlock(philo->right_fork);
 			usleep(params->eat_time * 1000);
-			pthread_mutex_lock(&params->protect_printf);
-			c_time = get_current_time() - params->start_time;
+			pthread_mutex_lock(&params->protect_dead);
 			if (params->p_dead == 0)
+			{
+				pthread_mutex_lock(&params->protect_printf);
+				c_time = get_current_time() - params->start_time;
 				printf("%ld %d is sleeping\n", c_time, philo->index);
-			pthread_mutex_unlock(&params->protect_printf);
+				pthread_mutex_unlock(&params->protect_printf);
+			}
+			pthread_mutex_unlock(&params->protect_dead);
 			usleep(params->sleep_time * 1000);
-			pthread_mutex_lock(&params->protect_printf);
-			c_time = get_current_time() - params->start_time;
+			pthread_mutex_lock(&params->protect_dead);
 			if (params->p_dead == 0)
+			{
+				pthread_mutex_lock(&params->protect_printf);
+				c_time = get_current_time() - params->start_time;
 				printf("%ld %d is thinking\n", c_time, philo->index);
-			pthread_mutex_unlock(&params->protect_printf);
+				pthread_mutex_unlock(&params->protect_printf);
+			}
+			pthread_mutex_unlock(&params->protect_dead);
 		}
 }
 
