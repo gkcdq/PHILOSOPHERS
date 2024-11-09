01c20c8b6b313d3374aceaeb0203effa2f67605c
gkcdq <thomasmilin1@gmail.com>
2024-11-09 05:10:22 +0100

    home

--- philo/routine.c
+++ philo/routine.c
@@ -44,7 +44,7 @@ void	impair_forks(t_philo *philo, t_params *params, long int c_time)
 		pthread_mutex_unlock(&params->protect_printf);
 }
 
-void	after_taking_forks(t_philo *philo, t_params *params, long int c_time)
+void	after_taking_forks_impair(t_philo *philo, t_params *params, long int c_time)
 {
 		philo->last_eat = get_current_time();
 		philo->count_eat += 1;
@@ -53,8 +53,8 @@ void	after_taking_forks(t_philo *philo, t_params *params, long int c_time)
 		if (params->p_dead == 0)
 			printf("%ld %d is eating\n", c_time, philo->index);
 		pthread_mutex_unlock(&params->protect_printf);
-		pthread_mutex_unlock(philo->left_fork);
 		pthread_mutex_unlock(philo->right_fork);
+		pthread_mutex_unlock(philo->left_fork);
 		usleep(params->eat_time * 1000);
 		pthread_mutex_lock(&params->protect_printf);
 		c_time = get_current_time() - params->start_time;
@@ -67,6 +67,37 @@ void	after_taking_forks(t_philo *philo, t_params *params, long int c_time)
 		if (params->p_dead == 0)
 			printf("%ld %d is thinking\n", c_time, philo->index);
 		pthread_mutex_unlock(&params->protect_printf);
+
+}
+
+void	after_taking_forks(t_philo *philo, t_params *params, long int c_time)
+{
+		if (philo->index % 2 != 0)
+			after_taking_forks_impair(philo, params, c_time);
+		else
+		{
+			philo->last_eat = get_current_time();
+			philo->count_eat += 1;
+			pthread_mutex_lock(&params->protect_printf);
+			c_time = get_current_time() - params->start_time;
+			if (params->p_dead == 0)
+				printf("%ld %d is eating\n", c_time, philo->index);
+			pthread_mutex_unlock(&params->protect_printf);
+			pthread_mutex_unlock(philo->left_fork);
+			pthread_mutex_unlock(philo->right_fork);
+			usleep(params->eat_time * 1000);
+			pthread_mutex_lock(&params->protect_printf);
+			c_time = get_current_time() - params->start_time;
+			if (params->p_dead == 0)
+				printf("%ld %d is sleeping\n", c_time, philo->index);
+			pthread_mutex_unlock(&params->protect_printf);
+			usleep(params->sleep_time * 1000);
+			pthread_mutex_lock(&params->protect_printf);
+			c_time = get_current_time() - params->start_time;
+			if (params->p_dead == 0)
+				printf("%ld %d is thinking\n", c_time, philo->index);
+			pthread_mutex_unlock(&params->protect_printf);
+		}
 }
 
 void	eating_sleeping(t_philo *philo, t_params *params)
