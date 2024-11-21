397ab3866d2b3763626f84786a1507d2d8de118f
Thomas Milin <tmilin@made-f0Cr2s1.clusters.42paris.fr>
2024-11-21 19:12:11 +0100

    42

--- philo/sleep_t_sup_die_t.c
+++ philo/sleep_t_sup_die_t.c
@@ -17,12 +17,15 @@ void	sleep_time_sup_die_time(t_philo *philo, t_params *params)
 	while (1)
 	{
 		pthread_mutex_lock(&params->mutex_dead);
+		pthread_mutex_lock(&params->protect_dead);
 		if (params->p_dead)
 		{
 			pthread_mutex_unlock(&params->mutex_dead);
+			pthread_mutex_unlock(&params->protect_dead);
 			return ;
 		}
 		pthread_mutex_unlock(&params->mutex_dead);
+		pthread_mutex_unlock(&params->protect_dead);
 		eating_sleeping_v3(philo, params);
 	}
 }
